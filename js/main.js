7869580b608f4f189e9df5181275bafe77d4e165
junior <zukisanante@gmail.com>
2019-06-17 13:45:57 +0200

    results output modified but don't work as required yet

--- js/main.js
+++ js/main.js
@@ -7,7 +7,16 @@ function score(){
     var score_div = document.getElementById("score").innerHTML = UserPoints + " - " + ComputerPoints;
 }
 setInterval(score, 50);
+function convert(word){
+    if(word === "paper") return <i class="far fa-hand-paper"></i>;
+    if(word === "rock") return <i class="far fa-hand-rock"></i>;
+     return <i class="far fa-hand-scissors"></i>;
+}
 function game(UserChoice){
     var box = document.getElementById("challenge");
     box.style.display = "inline-flex";
+    var userDiv = document.getElementById("YourObject");
+    userDiv.innerHTML = convert(UserChoice);
+    var comDiv = document.getElementById("ComObject");
+    comDiv.innerHTML = convert(ComChoice);
 }
